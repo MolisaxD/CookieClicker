0cda906a0c28fa29d1f0189d7b83dc041a6efc5f
MELISSA SOUZA BORGES <mborges@labcetec.net>
2023-10-18 10:12:29 -0300

    putz

---
+++ src/cookieclicker/CookieClicker.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cookieclicker;
+
+import telas.CookieClickerTela;
+
+/**
+ *
+ * @author mborges
+ */
+public class CookieClicker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CookieClickerTela tela = new CookieClickerTela();
+        tela.setVisible(true);
+    }
+    
+}
